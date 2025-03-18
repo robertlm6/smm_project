b35e485d2bd615906cce9d0d8b094a39a0f1e846
DESKTOP-1P9MEJT\rober <robertolopez612@gmail.com>
2025-03-18 21:45:13 +0100

    SMM Practice 4: Basic Paint

---
+++ src/practica4_smm/HerramientaDibujo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package practica4_smm;
+
+/**
+ *
+ * @author rober
+ */
+public enum HerramientaDibujo {
+    LINE,
+    RECTANGLE,
+    ELLIPSE
+}
